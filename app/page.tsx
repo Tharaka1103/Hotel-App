82823d6933950fd7a5f792cf9e38a0d599a2b895
Tharaka1103 <supuntharakapro999@gmail.com>
2025-05-04 10:51:39 +0530

    Remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useRef } from "react";
 import Image from "next/image";
-import { motion, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import {
   Waves,
   Anchor,
